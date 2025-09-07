7a173e0bd55482df5fcf26a39d2894d27d0ad740
Selion11 <gdecaro@itba.edu.ar>
2025-09-07 19:39:29 -0300

    Code style refactoring

--- itba/converter/src/test/java/edu/itba/converter/exchange/CurrencyTest.java
+++ itba/converter/src/test/java/edu/itba/converter/exchange/CurrencyTest.java
@@ -1,6 +1,5 @@
 package edu.itba.converter.exchange;
 
-import edu.itba.converter.exchange.exception.UnableToConvertException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
--- itba/converter/src/test/java/edu/itba/converter/exchange/UnirestHttpClientTest.java
+++ itba/converter/src/test/java/edu/itba/converter/exchange/UnirestHttpClientTest.java
@@ -1,28 +1,21 @@
 package edu.itba.converter.exchange;
 
-import edu.itba.converter.exchange.exception.UnableToConvertException;
-import edu.itba.converter.exchange.exception.UnavailableRateService;
 import edu.itba.converter.exchange.httpclient.UnirestHttpClient;
-import edu.itba.converter.exchange.interfaces.RateGetter;
 import kong.unirest.core.GetRequest;
 import kong.unirest.core.Unirest;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
-import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class UnirestHttpClientTest {
 
 
     @Test
-    void testGetReturnsExpectedHttpResponse() { //TODO
+    void testGetReturnsExpectedHttpResponse() {
         // GIVEN
         String url = "http://test.com";
         Map<String, Object> queryParams = Map.of("q", "test");
@@ -36,18 +29,19 @@ public class UnirestHttpClientTest {
         when(mockRequest.queryString(queryParams)).thenReturn(mockRequest);
         when(mockRequest.headers(headers)).thenReturn(mockRequest);
         when(mockRequest.asString()).thenReturn(mockResponse);
-
+        HttpResponse response;
         // WHEN
         try (MockedStatic<Unirest> unirestMock = mockStatic(Unirest.class)) {
             unirestMock.when(() -> Unirest.get(url)).thenReturn(mockRequest);
 
             UnirestHttpClient client = new UnirestHttpClient();
-            edu.itba.converter.exchange.HttpResponse response = client.get(url, queryParams, headers);
-
-            // THEN
-            assertEquals(200, response.status());
-            assertEquals("{\"result\":\"ok\"}", response.body());
+            response = client.get(url, queryParams, headers);
         }
+
+        // THEN
+        assertEquals(200, response.status());
+        assertEquals("{\"result\":\"ok\"}", response.body());
+
     }
 
     @Test
@@ -56,17 +50,19 @@ public class UnirestHttpClientTest {
         String url = "http://test.com";
         Map<String, Object> queryParams = Map.of();
         Map<String, String> headers = Map.of();
+        HttpResponse response;
 
+        //WHEN
         try (MockedStatic<Unirest> unirestMock = mockStatic(Unirest.class)) {
-            //WHEN
+
             unirestMock.when(() -> Unirest.get(url)).thenThrow(new RuntimeException("Network error"));
 
             UnirestHttpClient client = new UnirestHttpClient();
-            edu.itba.converter.exchange.HttpResponse response = client.get(url, queryParams, headers);
-
-            //THEN
-            assertEquals(500, response.status());
-            assertEquals("Internal Server Error", response.body());
+            response = client.get(url, queryParams, headers);
         }
+
+        //THEN
+        assertEquals(500, response.status());
+        assertEquals("Internal Server Error", response.body());
     }
 }
